a03cd706db7dee9f17fdea7e7b01d6d4b6613c54
Deoncn <2210633536@qq.com>
2025-08-20 02:17:58 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 const app = createApp(App)
-
-
-import './assets/main.css'
-import './assets/DPlayer.min.css'
-
-// vue3-video-play
-// import vue3videoPlay from 'vue3-video-play' // 引入组件
-// import 'vue3-video-play/dist/style.css' // 引入css
-app.use()
 app.mount('#app')
+
+import '@/assets/main.css'
+import '@/assets/DPlayer.min.css'
